bff57b2ce7998b093f51e84a824636ed6766896d
Andre-Rodrigues-Correia <andrecorreiati@gmail.com>
2022-08-05 01:06:03 -0300

    update api games - config heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 //mongo - database
 const mongoose = require("mongoose")
 
-
+const PORT = process.env.PORT || 9090;
 //dados database
 const DB_USER = 'database-api-games';
 const DB_PASS =  encodeURIComponent('SYdQKUkmDZ0NnfAD')
@@ -41,7 +41,7 @@ mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@clusterapi.gmzbh6s.mongodb
 .then(() => {
     console.log('conectamos ao banco de dados')
     //porta api
-    app.listen(9090)
+    app.listen(PORT)
 })
 .catch((err) => {
     console.log(err)
